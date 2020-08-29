2c2d02447484021ae046f35085197e04b0a3d2d6
Janzent Vapor <43449794+developer-jx@users.noreply.github.com>
2020-08-29 17:16:14 +0800

    basic CRUD

--- app.py
+++ app.py
@@ -4,22 +4,60 @@ from flask_restful import Resource, Api
 # Resource represents an abstract REST resource
 
 app = Flask(__name__)
-
 api = Api(app)
 
-# Create a resource
-class HelloWorld(Resource):
-    # This is where you specify GET, POST, etc.
+# In this example, we'll only be using a List to return data.
+# But later on we'll be using a database.
+
+# For now, we'll have puppies in a list of dictionaries.
+# Example content: {'name':"Rufus"}
+puppies = []
+
+class PuppyNames(Resource):
+# All functions/definitions need to have the same parameter.
+# So if you'll have (self) as a parameter it must be applied to all.
+
+    # Get the puppies
+    def get(self, name):
+        # Get all puppies
+        for puppy in puppies:
+            # In every dictionary, get the pair data.
+            if puppy['name'] == name:
+                return puppy
+
+        # Only executes if it's empty
+        # Returns a 404 HTTP Error Code
+        return {'name':None}, 404
+
+    # Add a puppy
+    def post(self, name):
+        # Create an instance of a dictionary, then append it into the list.
+        add_pup = {'name':name}
+        puppies.append(add_pup)
+
+        return add_pup
+
+    # Delete a puppy
+    def delete(self, name):
+
+        # Reads all puppies and numbers them
+        for index, puppy in enumerate(puppies):
+            # Then look for the puppy with the name given
+            if puppy['name'] == name:
+                # Out of the index of the puppy, delete/pop it.
+                del_pup = puppies.pop(index)
+                return {'note':'delete success'}
+
+# This gets the list of ALL puppies
+class AllNames(Resource):
+
     def get(self):
-        # Returned in JSON
-        return {'hello':'world'}
+        # Simply returns every puppy
+        return {'puppies':puppies}
 
-# Note that HelloWorld =/= HelloWorld()
-# So in this case it's not creating an instance of the class HelloWorld
-# It is simply directly passing in the class itself
-api.add_resource(HelloWorld, '/')
-# api.add_resource(<class_resource>, <route>)
-
+# Register the resources.
+api.add_resource(PuppyNames, '/puppy/<string:name>')
+api.add_resource(AllNames, '/puppies')
 
 if __name__ == "__main__":
     app.run(debug=True)
